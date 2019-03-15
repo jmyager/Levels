e22edc3b372ae9557c600104be41435338225957
jmyager <jmyager6@gmail.com>
2019-03-15 13:17:30 -0400

    trail filter hotfix

--- public/assets/js/tournamentsResults.js
+++ public/assets/js/tournamentsResults.js
@@ -1,4 +1,3 @@
-"usestrict";
 
 var txBatch = [];
 var currentBatch = [];
@@ -45,7 +44,7 @@ function populateFilter(data) {
                 }
                 if ($.inArray(organizer + " - " + trail, dupeArray) === -1) {
                     var option = $("<option>" + organizer + " - " + trail + "</option>");
-                    $(option).attr("data-id", organizer + " - " + trail);
+                    $(option).attr("data-id", trail);
                     $("#trailSelect").append(option);
                     // Track the new option in dupeArray
                     dupeArray.push(organizer + " - " + trail);
@@ -257,7 +256,6 @@ $("#clearSubmit").on("click", function (e) {
     $("#addFilterSubmit").hide();
     $("#newFilterSubmit").hide();
     currentBatch = txBatch;
-    console.log(txBatch);
     displayFlatData(txBatch);
     $("#filterWrapper").toggle();
     filtered = false;
