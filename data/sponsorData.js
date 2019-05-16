c79c083c6a934dcd27adccc5b03f33b5dccbab5a
jmyager <jmyager6@gmail.com>
2019-05-16 17:02:38 -0400

    updated v2 tx-ad banner images

--- data/sponsorData.js
+++ data/sponsorData.js
@@ -37,28 +37,28 @@ module.exports = [
     {
         sponsor: "CATT Championship",
         href: "",
-        src: "/static/assets/img/tournament-banners/catt-academy-sports-championship-kerr-6-1.png",
+        src: "/static/assets/img/tournament-banners/catt-academy-sports-championship-kerr-6-1-v2.png",
         location: ["kerr"],
         type: "tournament"
     },
     {
         sponsor: "CATT Jordan 5-18",
         href: "",
-        src: "/static/assets/img/tournament-banners/catt-oldnorth-jordan-5-18.png",
+        src: "/static/assets/img/tournament-banners/catt-oldnorth-jordan-5-18-v2.png",
         location: ["jordan"],
         type: "tournament"
     },
     {
         sponsor: "CATT Kerr 6-8",
         href: "",
-        src: "/static/assets/img/tournament-banners/catt-oldnorth-kerr-6-8.png",
+        src: "/static/assets/img/tournament-banners/catt-oldnorth-kerr-6-8-v2.png",
         location: ["kerr"],
         type: "tournament"
     },
     {
         sponsor: "CATT Jordan 6-29",
         href: "",
-        src: "/static/assets/img/tournament-banners/catt-oldnorth-jordan-6-29.png",
+        src: "/static/assets/img/tournament-banners/catt-oldnorth-jordan-6-29-v2.png",
         location: ["jordan"],
         type: "tournament"
     }
